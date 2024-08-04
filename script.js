146a8dd9bd4a9c5fef07499b75a4f9f9d0a87e2f
ichsan <alfarizi443@gmail.com>
2024-08-04 22:56:27 +0700

    add identitas kelompok

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// Anggota Kelompok  
+//   Nama : Ichsanul Fikri Al Farisi 
+//   Nim : 230401010333
+//   Kelas : IT203
+//   Mata Kuliah : Pemrograman Web
+
+//   Nama : Abdurrahman Rizki  
+//   Nim  : 230401010078  
+//   Kelas : IT203
+//   Mata Kuliah : Pemrograman Web
+
+
 // modal script
 const closeModalBtn = document.getElementById("modal-close")
 const overlay = document.getElementById("overlay")
